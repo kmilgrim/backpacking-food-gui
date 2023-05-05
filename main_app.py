dc070f5f62ac076003771670c4bfc7cc237b8029
kmilgrim <kmilgrim@bu.edu>
2023-05-04 21:03:42 -0400

    scrollable frame that will show all added meals works

--- main_app.py
+++ main_app.py
@@ -11,15 +11,41 @@ from user_handle import *
 MAIN_APP = None
 
 
+# create check box frame for showing all meals
+class ScrollableCheckBoxFrame(customtkinter.CTkScrollableFrame):
+    def __init__(self, master, item_list, command=None, **kwargs):
+        super().__init__(master, **kwargs)
+
+        self.command = command
+        self.checkbox_list = []
+        for i, item in enumerate(item_list):
+            self.add_item(item)
+
+    def add_item(self, item):
+        checkbox = customtkinter.CTkCheckBox(self, text=item)
+        if self.command is not None:
+            checkbox.configure(command=self.command)
+        checkbox.grid(row=len(self.checkbox_list), column=0, pady=(0, 10))
+        self.checkbox_list.append(checkbox)
+
+    def remove_item(self, item):
+        for checkbox in self.checkbox_list:
+            if item == checkbox.cget("text"):
+                checkbox.destroy()
+                self.checkbox_list.remove(checkbox)
+                return
+
+    def get_checked_items(self):
+        return [checkbox.cget("text") for checkbox in self.checkbox_list if checkbox.get() == 1]
+
+
 class App(customtkinter.CTk):
+
     def handle_login(self, username: str, password: str) -> None:
         if login(username, password):
             tkinter.messagebox.showinfo('Success', 'Login Success')
 
-            self.login_frame.destroy()
-            self.main_frame()
-            createMeal("fried pickes", "pickles and fries",
-                       10000, 3, 78, "today", "today")
+            self.create_main_frame()
 
         else:
             tkinter.messagebox.showinfo(
@@ -65,6 +91,36 @@ class App(customtkinter.CTk):
 
     def main_frame(self):
         create_meal_table()
+
+    def checkbox_frame_event(self):
+        print(
+            f"checkbox frame modified: {self.scrollable_checkbox_frame.get_checked_items()}")
+
+    def create_main_frame(self):
+        self.login_frame.destroy()
+        self.main_frame()
+
+        # create scrollable checkbox frame to show meals
+        self.scrollable_checkbox_frame = ScrollableCheckBoxFrame(master=self, width=200, height=300, command=self.checkbox_frame_event,
+                                                                 item_list=[f"meal {i}" for i in range(3)])
+        self.scrollable_checkbox_frame.grid(
+            row=0, column=0, padx=15, pady=15, sticky="ns")
+        self.scrollable_checkbox_frame.add_item("new item")
+
+        # button to add meals
+        # self.button_add_meal = customtkinter.CTkButton(
+        #     master=self.main_frame, text="Add a Meal", command=self.meal_add, width=200, height=20)
+        # self.button_add_meal.pack(padx=20, pady=10)
+        # self.button_add_meal.place(x=100, y=350)
+
+        # get the details entered by user
+
+        # create meal with details
+        createMeal("fried pickes", "pickles and fries",
+                   10000, 3, 78, "today", "today")
+
+    def meal_add(self):
+        print("yes")
 
 
 if __name__ == "__main__":
